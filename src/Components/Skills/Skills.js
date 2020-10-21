11946a2395e7952c4a0b5efedf0d951bc1a465e6
Brandon Berke <60757399+warbrain11@users.noreply.github.com>
2020-10-21 16:32:26 -0400

    More intro page size fixes

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -13,145 +13,147 @@ const Skills = (props) =>
         <div className = {styles.Skills}>
             <div className = {styles.Background}/>
 
-            <div className = {styles.SkillsContent}>
-                <h2>SKILLS</h2>
-                <GlobalNav />
-
-                <hr/>
-                <h3 className = {styles.Heading}>EDUCATION</h3>
-                    <div className = {styles.Education}>
-                        
-                        
-                        {
-                            myContext.colleges.map((college, index) => {
+                <div className = {styles.SkillsContainer}> 
+                    <div className = {styles.SkillsContent}>
+                        <h2>SKILLS</h2>
+                        <GlobalNav />
+    
+                        <hr/>
+                        <h3 className = {styles.Heading}>EDUCATION</h3>
+                            <div className = {styles.Education}>
                                 
-                                return(
-                                    <div className = {styles.College} key = {'college' + index + college.name}>
-                                        <img src = {require(`../../assets/${college.image}`)} alt = "college"/>
-
-                                        <div className = {styles.CollegeDetails}> 
-
-                                            <label className = {styles.CollegeName}>{college.name.toUpperCase()}</label>
-                                            <div className = {styles.CollegeDetailsRow}>
-                                                
-                                                <label>DEGREE: </label>
-                                                <span>{college.degree}</span>
-                                            </div>
-
-                                            <div className = {styles.CollegeDetailsRow}>
-                                                
-                                                <label>CONCENTRATION: </label>
-                                                <span>{college.concentration}</span>
-                                            </div>
-
-                                            <div className = {styles.CollegeDetailsRow}>
-                                                
-                                                <label>DEGREE TYPE: </label>
-                                                <span>{college.degreeType}</span>
-                                            </div>
-
-                                            <div className = {styles.CollegeDetailsRow}>
-                                                
-                                                <label>GPA: </label>
-                                                <span>{college.gpa}</span>
-                                            </div>
-
-                                            <div className = {styles.CollegeDetailsRow}>
-                                                
-                                                <label>GRADUATED: </label>
-                                                <span>{college.graduated}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                );
-                            })
-                        }
-                    </div>
-
-                    
-                    <h3 className = {styles.Heading}>SKILLSET</h3>
-                
-                    <div className = {styles.Table}>
-                        <div className = {styles.Column}>
-                            <h3>
-                                LANGUAGES
-                            </h3>
-
-                            {myContext.skills.languages.map((language, index) =>
-                            {
-                              
-                                return (
-                                <div className = {styles.Skill} key = {'languageSkill' + index}>
-                                    <label>{language}</label>
-                                </div>);
-                            })}
+                                
+                                {
+                                    myContext.colleges.map((college, index) => {
+                                        
+                                        return(
+                                            <div className = {styles.College} key = {'college' + index + college.name}>
+                                                <img src = {require(`../../assets/${college.image}`)} alt = "college"/>
     
-                        </div>
+                                                <div className = {styles.CollegeDetails}> 
     
-                        <div className = {styles.Column}>
-                            <h3>
-                                FRAMEWORKS
-                            </h3>
+                                                    <label className = {styles.CollegeName}>{college.name.toUpperCase()}</label>
+                                                    <div className = {styles.CollegeDetailsRow}>
+                                                        
+                                                        <label>DEGREE: </label>
+                                                        <span>{college.degree}</span>
+                                                    </div>
     
-                            {myContext.skills.frameworks.map((framework, index) =>
-                            {
-                              
-                                return (
-                                <div className = {styles.Skill} key = {'frameworkSkill' + index}>
-                                    <label>{framework}</label>
-                                </div>);
-                            })}
-                        </div>
+                                                    <div className = {styles.CollegeDetailsRow}>
+                                                        
+                                                        <label>CONCENTRATION: </label>
+                                                        <span>{college.concentration}</span>
+                                                    </div>
     
-                        <div className = {styles.Column}>
-                            <h3>
-                                DATABASES
-                            </h3>
-
-                            {myContext.skills.databases.map((database, index) =>
-                            {
-                              
-                                return (
-                                <div className = {styles.Skill} key = {'databaseSkill' + index}>
-                                    <label>{database}</label>
-                                </div>);
-                            })}                          
-                        </div>
+                                                    <div className = {styles.CollegeDetailsRow}>
+                                                        
+                                                        <label>DEGREE TYPE: </label>
+                                                        <span>{college.degreeType}</span>
+                                                    </div>
     
-                        <div className = {styles.Column}>
-                            <h3>
-                                OS
-                            </h3>
+                                                    <div className = {styles.CollegeDetailsRow}>
+                                                        
+                                                        <label>GPA: </label>
+                                                        <span>{college.gpa}</span>
+                                                    </div>
+    
+                                                    <div className = {styles.CollegeDetailsRow}>
+                                                        
+                                                        <label>GRADUATED: </label>
+                                                        <span>{college.graduated}</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </div>
+    
                             
-                            {myContext.skills.os.map((os, index) =>
-                            {
-                              
-                                return (
-                                <div className = {styles.Skill} key = {'osSkill' + index}>
-                                    <label>{os}</label>
-                                </div>);
-                            })} 
-                           
-                        </div>
-
-                        <div className = {styles.Column}>
-                            <h3>
-                                PROJECT MANAGEMENT
-                            </h3>
-                            
-                            {myContext.skills.projectManagement.map((projectManagement, index) =>
-                            {
-                              
-                                return (
-                                <div className = {styles.Skill} key = {'projectManagementSkill' + index}>
-                                    <label>{projectManagement}</label>
-                                </div>);
-                            })} 
-                           
-                        </div>
+                            <h3 className = {styles.Heading}>SKILLSET</h3>
+                        
+                            <div className = {styles.Table}>
+                                <div className = {styles.Column}>
+                                    <h3>
+                                        LANGUAGES
+                                    </h3>
+    
+                                    {myContext.skills.languages.map((language, index) =>
+                                    {
+                                    
+                                        return (
+                                        <div className = {styles.Skill} key = {'languageSkill' + index}>
+                                            <label>{language}</label>
+                                        </div>);
+                                    })}
+            
+                                </div>
+            
+                                <div className = {styles.Column}>
+                                    <h3>
+                                        FRAMEWORKS
+                                    </h3>
+            
+                                    {myContext.skills.frameworks.map((framework, index) =>
+                                    {
+                                    
+                                        return (
+                                        <div className = {styles.Skill} key = {'frameworkSkill' + index}>
+                                            <label>{framework}</label>
+                                        </div>);
+                                    })}
+                                </div>
+            
+                                <div className = {styles.Column}>
+                                    <h3>
+                                        DATABASES
+                                    </h3>
+    
+                                    {myContext.skills.databases.map((database, index) =>
+                                    {
+                                    
+                                        return (
+                                        <div className = {styles.Skill} key = {'databaseSkill' + index}>
+                                            <label>{database}</label>
+                                        </div>);
+                                    })}                          
+                                </div>
+            
+                                <div className = {styles.Column}>
+                                    <h3>
+                                        OS
+                                    </h3>
+                                    
+                                    {myContext.skills.os.map((os, index) =>
+                                    {
+                                    
+                                        return (
+                                        <div className = {styles.Skill} key = {'osSkill' + index}>
+                                            <label>{os}</label>
+                                        </div>);
+                                    })} 
+                                
+                                </div>
+    
+                                <div className = {styles.Column}>
+                                    <h3>
+                                        PROJECT MANAGEMENT
+                                    </h3>
+                                    
+                                    {myContext.skills.projectManagement.map((projectManagement, index) =>
+                                    {
+                                    
+                                        return (
+                                        <div className = {styles.Skill} key = {'projectManagementSkill' + index}>
+                                            <label>{projectManagement}</label>
+                                        </div>);
+                                    })} 
+                                
+                                </div>
+                            </div>
+                        
                     </div>
-                
-            </div>
+                </div>
         </div>
     );
 }
